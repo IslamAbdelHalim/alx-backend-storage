75e9c22dcd6bbf021341e5deb5e44080717025b2
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-20 00:38:55 +0300

    a

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,42 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional, Any
+from functool import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Count methods calls decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper method"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Call history  methods decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper method"""
+        output = method(self, *args, **kwargs)
+        method_name = method.__qualname__
+        self._redis.rpush(f"{method_name}:inputs", str(args))
+        self._redis.rpush(f"{method_name}:outputs", str(output))
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> Callable:
+    """methods replay decorator"""
+    r = redis.Redis()
+    method_name = method.__qualname__
+    count = r.get(method_name).decode("utf-8")
+    inputs = r.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = r.lrange(f"{method_name}:outputs", 0, -1)
+    print(f"{method_name} was called {count} times:")
+    for i, o in zip(inputs, outputs):
+        print(f"{method_name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
 
 
 class Cache(object):
@@ -15,6 +51,8 @@ class Cache(object):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             generate random key
@@ -23,6 +61,7 @@ class Cache(object):
         self._redis.set(key, data)
         return key
 
+    @staticmethod
     def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         """Function that return a value"""
         value = self._redis.get(key)
@@ -30,6 +69,7 @@ class Cache(object):
             return fn(value)
         return value
 
+    @staticmethod
     def get_str(self, value: bytes) -> str:
         """to get string"""
         return str(value)
